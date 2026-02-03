356264c81ca9e6875447639b14a949daf4c3f159
NiaNurulCahya <nia0808772@gmail.com>
2026-02-03 10:26:41 +0700

    memisahkan fungsi proses dan cetak. memasukkan data ke array

--- main.js
+++ main.js
@@ -1,45 +1,32 @@
+let dataBarang = [] //aray global untuk menyimpan data barang 
+
 // fungsi format rupiah 1.000.000
 function rupiah(angka) {
   return angka.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
 function process() {
-   let JUMLAH_BARANG = Number(prompt("Masukan total belanja barang")) 
+  //reset data barang setiap kali proses di jalankan
+  dataBarang = [] 
   
-  let total = 0;
-  let output = "";
+  //reset output setiap kali proses harga dijalankan
+  document.getElementById("hasil").innerHTML = ""
+  
+   let JUMLAH_BARANG = Number(prompt("Masukan total belanja barang")) 
   
   // LOOP INPUT HARGA BARANG
   for (let i = 1; i <= JUMLAH_BARANG; i++) {
     let harga = Number(prompt("Masukkan harga barang ke-" + i));
-    total += harga;
-    
-    output += "Barang ke-" + i + ": Rp " + rupiah(harga) + "<br>";
+    dataBarang.push(harga)
   }
   
-  // HITUNG DISKON
-  let diskon = 0;
-  if (total > 500000) {
-    diskon = total * 0.2;
-  }
+  //aktifkan tombol cetak
+  document.getElementById("cetak").disabled = false
   
-  let setelahDiskon = total - diskon;
+  //menampilkaninfo barang yang sudah diinput
+  document.getElementById("hasil").innerHTML = "Data barang berhasil di input.(jumlah barang:" + dataBarang.length + ") "
+}
+
+function cetak(){
   
-  // HITUNG PPN
-  let ppn = setelahDiskon * 0.1;
-  
-  // TOTAL BAYAR
-  let totalBayar = setelahDiskon + ppn;
-  
-  // OUTPUT RINGKASAN
-  output += `
-    <div class="ringkasan">
-      Total belanja: Rp ${rupiah(total)}<br>
-      Diskon: Rp ${rupiah(diskon)}<br>
-      PPN 10%: Rp ${rupiah(ppn)}<br>
-      Total bayar: Rp ${rupiah(totalBayar)}
-    </div>
-  `;
-  
-  document.getElementById("hasil").innerHTML = output;
 }
